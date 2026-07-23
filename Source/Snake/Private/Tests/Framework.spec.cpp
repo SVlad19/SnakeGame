76a3ffe92cd0c3de55c61e62ec0650da4d93e151
Vlad Semenov <mr.vladik.semeno3@gmail.com>
2026-07-23 11:58:28 +0300

    Add tests

---
+++ Source/Snake/Private/Tests/Framework.spec.cpp
@@ -0,0 +1,44 @@
+// Snake game copyright
+
+#if WITH_AUTOMATION_TESTS
+
+#include "CoreMinimal.h"
+#include "Misc/AutomationTest.h"
+#include "Tests/Utils/TestUtils.h"
+#include "Framework/SG_GameMode.h"
+#include "Framework/SG_Pawn.h"
+
+BEGIN_DEFINE_SPEC(FSnakeFramework, "Snake",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority)
+
+UWorld* World;
+END_DEFINE_SPEC(FSnakeFramework)
+
+void FSnakeFramework::Define()
+{
+    using namespace Snake::Test;
+
+    Describe("Framework",
+        [this]()
+        {
+            BeforeEach(
+                [this]()
+                {
+                    AutomationOpenMap("GameLevel");
+                    World = GetTestGameWorld();
+                });
+
+            It("GamMapMightExist", [this]() { TestNotNull("World exists", World); });
+            It("ClassesMightBeSetupCorrectly",
+                [this]()
+                {
+                    TestNotNull("Game mode set up", Cast<ASG_GameMode>(World->GetAuthGameMode()));
+                    TestNotNull("Snake pawn set up", Cast<ASG_Pawn>(World->GetFirstPlayerController()->GetPawn()));
+                });
+            xIt("PawnLocationShouldBeAdjustCorrectly", [this]() { 
+                
+                });
+        });
+}
+
+#endif
